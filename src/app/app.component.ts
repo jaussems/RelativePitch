c9595e0097a752328b74c9c466f4a1e168badb9b
Jannes <jannesaussems@gmail.com>
2025-02-26 22:44:48 +0100

    Able to play random note, now fixing playing using keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  title = 'relative-pitch';
-
+export class AppComponent{
   /**
    * FIXMe: make a loop in the future as this will be come quite big
    */
   allPianoNotes: Record<string, string> =  {
     "C4": "assets/audio/PianoC4.wav",
+    "C4#": "assets/audio/PianoC4_sharp.wav",
     "D4": "assets/audio/PianoD4.wav",
-    "D4#": "assets/audio/PianoD4#.wav",
+    "D4#": "assets/audio/PianoD4_sharp.wav",
     "E4": "assets/audio/PianoE4.wav",
     "F4": "assets/audio/PianoF4.wav",
-    "F4#": "assets/audio/PianoF4#.wav",
+    "F4#": "assets/audio/PianoF4_sharp.wav",
     "G4": "assets/audio/PianoG4.wav",
-    "G4#": "assets/audio/PianoG4#.wav",
+    "G4#": "assets/audio/PianoG4_sharp.wav",
     "A4": "assets/audio/PianoA4.wav",
-    "A4#": "assets/audio/PianoA4#.wav",
+    "A4#": "assets/audio/PianoA4_sharp.wav",
     "B4": "assets/audio/PianoB4.wav",
   }
 
-
-  ngOnInit() {
-
+  playPianoNote(note: string) {
+   var audio = new Audio();
+   audio.src = this.allPianoNotes[note];
+   audio.play();
   }
 
-  playRandomSound() {
+  playRandomNote() {
+    let randomNumber = Math.floor(Math.random() * Object.keys(this.allPianoNotes).length)
+    let randomPianoNote  = Object.keys(this.allPianoNotes)[randomNumber]
     var audio = new Audio();
-    audio.src = "assets/audio/PianoC4.wav";
+    audio.src = this.allPianoNotes[randomPianoNote];
     audio.play();
   }
 }
