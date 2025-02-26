a3a1a9a6dd2b24d09fd1cc2b9f86189a5f2ee0f1
Jannes <jannesaussems@gmail.com>
2025-02-26 21:50:12 +0100

    RP-003-Play-random-sounds-and-play-piano-on-click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'relative-pitch';
+
+  /**
+   * FIXMe: make a loop in the future as this will be come quite big
+   */
+  allPianoNotes: Record<string, string> =  {
+    "C4": "assets/audio/PianoC4.wav",
+    "D4": "assets/audio/PianoD4.wav",
+    "D4#": "assets/audio/PianoD4#.wav",
+    "E4": "assets/audio/PianoE4.wav",
+    "F4": "assets/audio/PianoF4.wav",
+    "F4#": "assets/audio/PianoF4#.wav",
+    "G4": "assets/audio/PianoG4.wav",
+    "G4#": "assets/audio/PianoG4#.wav",
+    "A4": "assets/audio/PianoA4.wav",
+    "A4#": "assets/audio/PianoA4#.wav",
+    "B4": "assets/audio/PianoB4.wav",
+  }
+
+
+  ngOnInit() {
+
+  }
+
+  playRandomSound() {
+    var audio = new Audio();
+    audio.src = "assets/audio/PianoC4.wav";
+    audio.play();
+  }
 }
